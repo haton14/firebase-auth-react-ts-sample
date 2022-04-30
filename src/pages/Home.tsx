61de265da854e23149af45e6ef1199e3e23d6d56
haton14 <iioka.takumi.quena@gmail.com>
2022-04-30 23:14:34 +0900

    未認証ならloginに飛ばす

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { getAuth, signOut } from 'firebase/auth';
+import { Navigate } from 'react-router-dom';
 
 interface HomeProps {
   token: string;
@@ -6,12 +7,14 @@ interface HomeProps {
 
 const Home = (props: HomeProps) => {
   const auth = getAuth();
-  return (
+  return auth.currentUser ? (
     <div>
       <p>Home Page</p>
       <p>idToken: {props.token}</p>
       <button onClick={() => signOut(auth)}>Sign out</button>
     </div>
+  ) : (
+    <Navigate to="/login" />
   );
 };
 

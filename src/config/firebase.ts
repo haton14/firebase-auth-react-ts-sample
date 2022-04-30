9a2062875ca4faa775f2268d454093eb352f6c95
haton14 <iioka.takumi.quena@gmail.com>
2022-04-30 21:13:29 +0900

    firebase 初期化

---
+++ src/config/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env['REACT_APP_FIREBASE_API_KEY'] as string,
+  authDomain: process.env['REACT_APP_FIREBASE_AUTH_DOMAIN'] as string,
+  projectId: process.env['REACT_APP_FIREBASE_PROJECT_ID'] as string,
+  storageBucket: process.env['REACT_APP_FIREBASE_STORAGE_BUCKET'] as string,
+  messagingSenderId: process.env['REACT_APP_FIREBASE_MESSAGING_SENDER_ID'] as string,
+  appId: process.env['REACT_APP_FIREBASE_APP_ID'] as string,
+  measurementId: process.env['REACT_APP_FIREBASE_MEASUREMENT_ID'] as string,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
